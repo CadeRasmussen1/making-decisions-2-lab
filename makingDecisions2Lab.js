903b3bfe4942ee000aadb38c9c22d5b6a85d9476
Cade Rasmussen <caderasmussen@gmail.com>
2021-11-11 13:39:11 -0700

    problem 2

--- makingDecisions2Lab.js
+++ makingDecisions2Lab.js
@@ -8,8 +8,9 @@ var arr = [10,20,30,40,50,60]
 
 //Code Here
 
-let index = 0
-console.log(arr[index])
+// let index = 0
+
+// console.log(arr[index])
 
 ////////// PROBLEM 2 //////////
 /*
@@ -17,6 +18,10 @@ console.log(arr[index])
 */
 
 //Code Here
+
+let lastItemRemoved = arr.pop()
+
+console.log(arr)
 
 ////////// PROBLEM 3 //////////
 
